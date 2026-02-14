ed51029adca10ddc7d89a1c1ac55b5b4ae6d5aa0
Ilham Rizki Mulyawan <ilhamrzkm@gmail.com>
2026-02-14 13:37:19 +0700

    hw0b: Completed ListExercises.java

--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,29 +1,58 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExercises {
 
     /** Returns the total sum in a list of integers */
     public static int sum(List<Integer> L) {
-        // TODO: Fill in this function.
-        return 0;
+        int sum = 0;
+
+        if (L.isEmpty()) {
+            return 0;
+        } else {
+            for (int n : L) {
+                sum = sum + n;
+            }
+            return sum;
+        }
     }
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
-        // TODO: Fill in this function.
-        return null;
+        List<Integer> output = new ArrayList<>();
+
+        for (int n : L) {
+            if (n % 2 == 0) {
+                output.add(n);
+            }
+        }
+        return output;
     }
 
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
-        // TODO: Fill in this function.
-        return null;
+        List<Integer> output = new ArrayList<>();
+
+        for (int i : L1) {
+            if (L2.contains(i)) {
+                output.add(i);
+            }
+        }
+        return output;
     }
 
 
     /** Returns the number of occurrences of the given character in a list of strings. */
     public static int countOccurrencesOfC(List<String> words, char c) {
-        // TODO: Fill in this function.
-        return 0;
+        int count = 0;
+
+        for (String word : words) {
+            for (char character : word.toCharArray()) {
+                if (c == character) {
+                    count += 1;
+                }
+            }
+        }
+        return count;
     }
 }
