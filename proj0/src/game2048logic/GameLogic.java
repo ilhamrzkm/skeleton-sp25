239e1ed9ddde58e76f35f032368a365c177f38b4
Ilham Rizki Mulyawan <ilhamrzkm@gmail.com>
2026-02-19 12:40:15 +0700

    proj0: Completed GameLogic.java, all test passed.

--- proj0/src/game2048logic/GameLogic.java
+++ proj0/src/game2048logic/GameLogic.java
@@ -19,7 +19,22 @@ public class GameLogic {
      *              if no merge occurs, then return 0.
      */
     public static int moveTileUpAsFarAsPossible(int[][] board, int r, int c, int minR) {
-        // TODO: Fill this in in tasks 2, 3, 4
+        if (r == 0) {
+            return 0;
+        }
+        // iterate from current tile to all the way up
+        for (int i = r; i > 0; i -= 1) {
+            // move up if above current tile board[r][c] is empty
+            if (board[i - 1][c] == 0 && i > minR) {
+                board[i - 1][c] = board[i][c];
+                board[i][c] = 0;
+                // merge up if adjacent tile is the same as current tile
+            } else if (board[i - 1][c] == board[i][c] && i > minR) {
+                board[i - 1][c] *= 2;
+                board[i][c] = 0;
+                return i;
+            }
+        }
         return 0;
     }
 
@@ -31,8 +46,13 @@ public class GameLogic {
      * @param c         the column to tilt up.
      */
     public static void tiltColumn(int[][] board, int c) {
-        // TODO: fill this in in task 5
-        return;
+        int minR = 0;
+        for (int i = 0; i < board.length; i += 1) {
+            if (board[i][c] == 0) {
+                continue;
+            }
+            minR = moveTileUpAsFarAsPossible(board, i, c, minR);
+        }
     }
 
     /**
@@ -41,8 +61,9 @@ public class GameLogic {
      * @param board     the current state of the board.
      */
     public static void tiltUp(int[][] board) {
-        // TODO: fill this in in task 6
-        return;
+        for (int i = 0; i < board.length; i += 1) {
+            tiltColumn(board, i);
+        }
     }
 
     /**
@@ -53,15 +74,23 @@ public class GameLogic {
      * @param side  the direction to tilt
      */
     public static void tilt(int[][] board, Side side) {
-        // TODO: fill this in in task 7
+
         if (side == Side.EAST) {
-            return;
+            rotateLeft(board);
+            tiltUp(board);
+            rotateRight(board);
         } else if (side == Side.WEST) {
-            return;
+            rotateRight(board);
+            tiltUp(board);
+            rotateLeft(board);
         } else if (side == Side.SOUTH) {
-            return;
+            rotateRight(board);
+            rotateRight(board);
+            tiltUp(board);
+            rotateLeft(board);
+            rotateLeft(board);
         } else {
-            return;
+            tiltUp(board);
         }
     }
 }
